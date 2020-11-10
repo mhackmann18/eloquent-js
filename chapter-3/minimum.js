b2147556bb4bd6e667d4af63b43ec2e702a1e828
Matt Hackmann <mhackmann13@icloud.com>
2020-11-09 18:44:18 -0600

    Added descriptions of the problem the exercise is referencing at the top of each file

--- chapter-3/minimum.js
+++ chapter-3/minimum.js
@@ -1,3 +1,9 @@
+/*
+ASSIGNMENT SPECS
+
+The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+*/
+
 function minimum(i, j){
   if(i > j) return j;
   else if(j > i) return i;
--- chapter-5/flattening.js
+++ chapter-5/flattening.js
@@ -1,3 +1,9 @@
+/*
+ASSIGNMENT SPECS
+
+Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+*/
+
 let arrayOfArrays = [
   [1,2,3,4,5],
   [11,22,33,44],
